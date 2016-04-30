54fdc2791a66f13657c56baa2896f09a3c7f7f46
piotrekwe <piotrekwe@gmail.com>
2016-04-30 16:14:53 +0200

    change comments from polish to english

--- import/importer_04.py
+++ import/importer_04.py
@@ -2,7 +2,7 @@ import urllib
 import time
 import sqlite3
 
-def rpso_generator(file_name) : # tworzy liste z numerami rspo szkol warszawskich
+def rpso_generator(file_name) :  # create list with rspo numbers of warsaw schools
     source=open(file_name)
     container=list()
     for item in source:
@@ -21,19 +21,19 @@ if len(source_name)<1 :
 
 rpso_data=rpso_generator(source_name)
 
-conn = sqlite3.connect('school_internet.sqlite') #otwieram baze danych
+conn = sqlite3.connect('school_internet.sqlite') #accessing database
 cur = conn.cursor()
 
 cur.execute('''
-CREATE TABLE IF NOT EXISTS Schools (rspo INTEGER, imported TEXT)''') # sprawdzam czy struktura bazy juz jest utworzona
+CREATE TABLE IF NOT EXISTS Schools (rspo INTEGER, imported TEXT)''') # checking if databases structure is already set
 
-for item in rpso_data : #interujemy po numerach rspo
+for item in rpso_data : #itering by rspo numbers
     current_rspo=item
     i=i+1
-    cur.execute("SELECT imported FROM Schools WHERE rspo= ?", (current_rspo, )) #sprawdza czy mamy juz w bazie rekord o danym numerze rspo
+    cur.execute("SELECT imported FROM Schools WHERE rspo= ?", (current_rspo, )) #checking if record for this rspos already exists
 
     try:
-        h = cur.fetchone()[0] #jezeli udalo sie pobrac ten element z bazy danych to przechodzimy do nastepnej iteracji
+        h = cur.fetchone()[0] #if there is an element with rspo_current value, we skip to next iteration
         if len(h)>1:
             print "Found in databes1: ", current_rspo
             continue
@@ -43,14 +43,14 @@ for item in rpso_data : #interujemy po numerach rspo
         pass
 
     rspo_value='''{"\ufeffNr RSPO":"'''+str(current_rspo)+'''"}''' # potrojne ciapki spowodowane obecnoscia ciapkow w srodku
-    url=service_url+urllib.urlencode({'filters': rspo_value }) # tworze adres do metody get
+    url=service_url+urllib.urlencode({'filters': rspo_value }) # create url adress for get method
     print "Retrieving information about school with RSPO: ", current_rspo
-    data=urllib.urlopen(url).read() #otwieram strone i wpisuja ja do stringa
-#powinna tu sie pojawic opcja walidacji pobranego contentu
+    data=urllib.urlopen(url).read() #opening webapge and writing it to string
+#ToDo: create some validation of downloaded content
     cur.execute('''INSERT INTO Schools (rspo, imported)
-            VALUES ( ?, ? )''', ( current_rspo, data ) ) # jestesmy w czesci w ktorej mamy pewnosci ze nie mamy duplikatow
+            VALUES ( ?, ? )''', ( current_rspo, data ) ) # now we are sure thare are no duplicates
 
-                                 #dodajemy pobrany wpis do bazy danych
+                                 #adding downloaded information to database
     conn.commit()
 
 print "Importing completed"
